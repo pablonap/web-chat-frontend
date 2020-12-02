60cca52138321de9619500cab2578e5e11e92064
chipo <pnapoli33@gmail.com>
2020-12-02 01:44:09 -0300

    video #207 - Configure Client in chat.component and establish the connection with the broker.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
+  private client: Client;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    this.client = new Client();
+    this.client.webSocketFactory = () => {
+      return new SockJS("http://localhost:8080/chat-websocket");
+    }
 
+    this.client.onConnect = (frame) => {
+      // frame object contains all the information of the connection with the broker.
+      console.log('Conectados: ' + this.client.connected + ' : ' + frame );
+    }
+
+    // Starts the connection
+    this.client.activate();
+
+  }
 }
