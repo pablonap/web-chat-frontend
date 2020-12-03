0eab2a10260bc654270752446edb0f04042e430c
chipo <pnapoli33@gmail.com>
2020-12-02 23:10:01 -0300

    video #210.2 - Implement "subscribe()" and "publish()" methods.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -9,7 +10,12 @@ import * as SockJS from 'sockjs-client';
 })
 export class ChatComponent implements OnInit {
   private client: Client;
+
   conectado: boolean = false;
+
+  mensaje: Mensaje = new Mensaje();
+
+  mensajes: Mensaje[] = [];
 
   constructor() {}
 
@@ -23,6 +29,14 @@ export class ChatComponent implements OnInit {
       // frame object contains all the information of the connection with the broker.
       console.log('Conectados: ' + this.client.connected + ' : ' + frame);
       this.conectado = true;
+
+      // "subscribe" can be taken also as "listen" to an event.
+      this.client.subscribe('/chat/mensaje', e => {
+        // This is a message written by someone which in turn was received by the broker and then sent to the clients.
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      })
     };
 
     this.client.onDisconnect = (frame) => {
@@ -40,4 +54,10 @@ export class ChatComponent implements OnInit {
   desconectar(): void {
     this.client.deactivate();
   }
+
+  enviarMensaje(): void {
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    this.mensaje.texto = '';
+  }
+
 }
